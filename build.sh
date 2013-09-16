d7443f047a5c89d5e7715947182d0fcfaaa72071
Will Kurt <wckurt@gmail.com>
2013-09-16 01:10:48 -0700

    moved js into src folder

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "minifiying"
-java -jar ~/closure/compiler.jar --js=constants.js --js=effects.js --js=backgrounds.js --js=main.js --js_output_file=lazerhead.min.js
+java -jar ~/closure/compiler.jar --js=src/constants.js --js=src/effects.js --js=src/backgrounds.js --js=src/main.js --js_output_file=lazerhead.min.js
