12a99fdc0bdccb9b2a5412bedcce63c277d39dc4
Will Kurt <wckurt@gmail.com>
2013-09-21 02:47:32 -0700

    added build file for windows

---
+++ build.bat
@@ -0,0 +1,1 @@
+java -jar "C:\Program Files (x86)\ClosureCompiler\compiler.jar" --js=src/constants.js --js=src/sound.js --js=src/effects.js --js=src/backgrounds.js --js=src/main.js --js_output_file=lazerhead.min.js
