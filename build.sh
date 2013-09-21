3f7cf5933d6218c787cf3b0cfdfb69f35d46b162
Will Kurt <wckurt@gmail.com>
2013-09-20 21:01:48 -0700

    pull sound out of main file

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "minifiying"
-java -jar ~/closure/compiler.jar --js=src/constants.js --js=src/effects.js --js=src/backgrounds.js --js=src/main.js --js_output_file=lazerhead.min.js
+java -jar ~/closure/compiler.jar --js=src/constants.js --js=src/sound.js --js=src/effects.js --js=src/backgrounds.js --js=src/main.js --js_output_file=lazerhead.min.js
---
+++ src/sound.js
@@ -0,0 +1,85 @@
+//audio
+//basic waves from: joshondesign.com/p/demos/sound/waveviz/
+var sin = Math.sin;
+function square(t){
+  var v = sin(t);
+  if(v < 0) return -1;
+  return 1;
+}
+
+function saw(t){
+  t = t/(2*Math.PI);
+  return (t - Math.floor(t))*2 -1;
+}
+
+function tone(t,freq){
+  return sin(t*2*Math.PI*freq);
+}
+
+
+var actx = new webkitAudioContext();
+var jsnode = actx.createScriptProcessor(512,0,1);
+var t = 0;
+jsnode.onaudioprocess = function(e) {
+    var output = e.outputBuffer.getChannelData(0);
+    for (var i = 0; i < output.length; i++) {
+        out = (lazerSFX(t)+
+                     playerInjuredSFX(t)+
+                     chargeSFX(t)
+              )/3;
+      output[i] = out;
+        t += 1/44000.0;
+    }
+}
+jsnode.connect(actx.destination);
+
+//refactor note: really should pull out all these state vars at somepoint
+var lazerFiring;
+function lazerSFX(t){
+  if(lazerFiring){
+    return (tone(t,261.63)+tone(t*1.03,261.63))/2;
+  } else {
+    return 0;
+  }
+}
+
+
+
+
+
+var playerInjured;
+function playerInjuredSFX(t){
+  if(playerInjured > 0){
+    return Math.random(t)+sin(t*2*Math.PI*260);
+  } else {
+    return 0;
+  }
+
+}
+
+var charget = 0.15;
+var chargeUp = false;
+function chargeSFX(t) {
+
+  if(charging){
+    if(charget > 0.3){
+      chargeUp = false
+    }
+    if(charget < 0.01){
+      chargeUp = true
+    }
+
+    if(!chargeUp){
+      charget -= 1/(44000.0*(1.1-player.power/player.maxpower));
+    } else {
+      charget += 1/(44000.0*(1.1-player.power/player.maxpower));
+    }
+    var freq = 260*(1+charget);
+    return (sin(charget*2*Math.PI*freq)+
+            sin(charget*2*Math.PI*freq*1.2)+
+            sin(charget*2*Math.PI*freq*1.7))/3;
+  }
+  return 0;
+}
+
+
